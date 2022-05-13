a8eff82712383c2ae4ba06b42697adebfadc5d53
leepenney <leepenney@gmail.com>
2022-05-13 10:52:49 +0100

    Updates

---
+++ precache-manifest.12e39cc483d0eea8b73147b66d4a2f51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3488667eb2210cad19ee0def44edd2",
+    "url": "/react-jobs-board-example/index.html"
+  },
+  {
+    "revision": "38dee42e9c5d0636b6fc",
+    "url": "/react-jobs-board-example/static/css/main.6d761f0f.chunk.css"
+  },
+  {
+    "revision": "7ae745461ee0e9e0c0c1",
+    "url": "/react-jobs-board-example/static/js/2.3694dc9f.chunk.js"
+  },
+  {
+    "revision": "77ff2abf6da91ad83241",
+    "url": "/react-jobs-board-example/static/js/3.153d6254.chunk.js"
+  },
+  {
+    "revision": "38dee42e9c5d0636b6fc",
+    "url": "/react-jobs-board-example/static/js/main.26694ab2.chunk.js"
+  },
+  {
+    "revision": "a0de65c602bb145e1093",
+    "url": "/react-jobs-board-example/static/js/runtime~main.eb1e3941.js"
+  },
+  {
+    "revision": "ca59d58bc7c11a927e5af85bfad89a15",
+    "url": "/react-jobs-board-example/static/media/example-rss.ca59d58b.xml"
+  }
+]);
